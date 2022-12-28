8d03f3556377f9f3b1f1df242e6ad9f328cce8ee
Mirza Abdul wadood Baig <baigwadood@gmail.com>
2022-12-28 15:16:30 +0500

    Update main.py.py
    changes model selection name

--- main.py.py
+++ main.py.py
@@ -19,7 +19,8 @@ if uploaded_file is not None:
     input = image.save("input.png")
     option = st.selectbox(
         'Select model',
-        ('u2net ', 'u2netp', 'u2net_human_seg','u2net_cloth_seg','silueta'))
+        #('u2net ', 'u2netp', 'u2net_human_seg','u2net_cloth_seg','silueta'))
+        ('General (For general use cases)','Quick Remove (Faster background removal for general use cases)','Human (Remove background from people)','Product (Remove background from products and cars)','Light Weight (Light-weight background removal for general use cases)'))
 
 st.write('You selected:', option)
 
@@ -29,8 +30,18 @@ if st.button("Remove"):
         #output = remove(bytes_data)
         #st.image(output, caption='Sunrise by the mountains')
         # Importing required module
+         if option=='General (For general use cases)':
+            model="u2net"
+        elif option== 'Quick Remove (Faster background removal for general use cases)':
+            model='u2netp'
+        elif option=='Human (Remove background from people)':
+            model= 'u2net_human_seg'
+        elif option == 'Product (Remove background from products and cars)':
+            model= 'u2net_cloth_seg'
+        elif option=='Light Weight (Light-weight background removal for general use cases)':
+            model= 'silueta'
 
-        os.system(f'rembg i -m {option} input.png output.png')
+        os.system(f'rembg i -m {model} input.png output.png')
         image = Image.open('output.png')
         st.image(image, caption='Background removed')
         with open("output.png", "rb") as file:
